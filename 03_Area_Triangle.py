f9d796ba33fe2b923f544fa66bb421084bb2e314
Kaushal Naik <naikkaushal2222@gmail.com>
2025-07-31 18:57:30 +0530

    feat: Area Of Triangle

---
+++ 03_Area_Triangle.py
@@ -0,0 +1,10 @@
+### Finding The Area Of Triangle
+
+# The area of a triangle is given by the formula:
+# Area = (base × height) / 2
+
+
+height = 5  # in cm
+base = 10  # in cm
+area = (base * height) / 2
+print("The area of the triangle is", area, "cm^2")
