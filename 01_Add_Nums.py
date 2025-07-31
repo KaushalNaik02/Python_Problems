fecc4787d776f655cecc976241d86f1f6c8ed705
Kaushal Naik <naikkaushal2222@gmail.com>
2025-07-31 16:23:08 +0530

    feat: Adding Two Numbers By Deafault variable and User Input

--- 01_Add_Nums.py
+++ 01_Add_Nums.py
@@ -1,3 +1,11 @@
+#### Adding Two Numbers By Using setting Default Variable
+
 a = 10
 b = 20
 print(a + b)
+
+#### Adding Two Numbers By Taking Input From User
+
+x = int(input("Enter the number 1: "))
+y = int(input("Enter the number 2: "))
+print(x+y)
