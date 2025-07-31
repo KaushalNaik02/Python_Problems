e1cef2b064f6772a58fd870bd48a875ee030bdb1
Kaushal Naik <naikkaushal2222@gmail.com>
2025-07-31 18:49:56 +0530

    feat:Finding Square Root

---
+++ 02_Square_root.py
@@ -0,0 +1,11 @@
+##### Finding Square Root
+
+## Exponential Method
+num = int(input("Enter the Number: "))
+print(f"The Square Root of the {num} is ", int(num ** 0.5))
+
+
+## Using Math Module
+import math
+num1 = int(input("Enter th Number: "))
+print(f"The Square Root of the {num1} is ", math.sqrt(num1))
